778e4674f3e8512d86a65263f604bcf121848ea2
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-05-08 18:58:26 -0500

    asdfasf

---
+++ src/api/fetchShow.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+
+
+export const fetchShow = () => {
+    return (axios
+      .get(
+        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+      ))
+      .then(res => {
+        
+        return(res.data)
+        
+    });
+    
+
+
+
+  };
+
+
+
+
